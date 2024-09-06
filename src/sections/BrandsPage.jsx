7f3a49afd409d10cd5dd901fae31cbdf76ed4425
abhinav <abhinav312003@gmail.com>
2024-09-06 18:47:27 +0530

    commit

--- src/sections/BrandsPage.jsx
+++ src/sections/BrandsPage.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
 
 const BrandsPage = () => {
-    useEffect(() => {
+    // Register ScrollTrigger with GSAP
+    useGSAP(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        // Animation for the text spans in the larger screen layout
         gsap.fromTo(
             '.brand-text span',
             {
@@ -15,9 +21,17 @@ const BrandsPage = () => {
                 duration: 0.8,
                 stagger: 0.3, // Stagger each line animation by 0.3 seconds
                 ease: 'power3.out',
+                scrollTrigger: {
+                    trigger: '.brand-text', 
+                    start: 'top 80%', 
+                    end: 'bottom 50%', 
+                    scrub: false, 
+                    markers:true,
+                    toggleActions: 'play pause resume reset'
+                },
             }
         );
-    }, []);
+    });
 
     return (
         <section className='padding-x'>
@@ -31,9 +45,11 @@ const BrandsPage = () => {
             {/* for larger screens */}
             <div className='max-sm:hidden text-[30px] md:text-[4vw] leading-[40px] md:leading-[4vw] font-nb flex flex-col justify-center w-full mt-[20px] brand-text'>
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We are a venture firm and digital agency
-                    Our</span> <span> mission is to transform founders' visions into</span>
-                <span>remarkable brands. Choose traditional</span> <span>compensation or an equity offset through our</span>
-                <span> Venture Model— your vision, your decision.</span>
+                    Our</span> 
+                <span>mission is to transform founders' visions into</span>
+                <span>remarkable brands. Choose traditional</span> 
+                <span>compensation or an equity offset through our</span>
+                <span>Venture Model— your vision, your decision.</span>
             </div>
             {/* for small screens */}
             <div className='sm:hidden text-[7vw] md:text-[4vw] leading-[8vw] md:leading-[4vw] font-nb flex flex-col justify-center w-full mt-[20px] brand-text '>
